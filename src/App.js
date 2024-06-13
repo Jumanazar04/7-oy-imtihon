32169b2bd0f109e7b97eed3e9956b27d3b182527
Jumanazar04 <yusupovjumanazar004@gmail.com>
2024-06-13 17:47:29 +0500

    fix: add products page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
 import GeneralLayout from './components/GeneralLayout';
 import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
 import CategoriesPage from './pages/CategoriesPage';
+import ProductsPage from './pages/ProductsPage';
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
         <Route path='/' element={<GeneralLayout />}>
           <Route index element={<HomePage />} />
           <Route path='/category' element={<CategoriesPage />} />
+          <Route path='/products' element={<ProductsPage />} />
         </Route>
         <Route path='/login'  element={<LoginPage />}/>
       </Routes>
